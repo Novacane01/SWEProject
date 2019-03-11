1cef83cea7a4d190b74c9e7cfc561eded3a7bdb0
gallevy98 <32230577+gallevy98@users.noreply.github.com>
2019-03-11 16:13:12 -0400

    Placeholder

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   db: {
-    uri: 'mongodb://gallevy:root11@ds129085.mlab.com:29085/bootcamp4', //place the URI of your mongo database here.
+    uri: 'mongodb://client:client1@ds011745.mlab.com:11745/tutor_matching_service', //place the URI of your mongo database here.
   },
   port: process.env.PORT || 8080
 };
