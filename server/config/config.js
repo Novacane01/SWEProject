7a7e6efa63b67bcb4ead7dcfab45eafe7a77487d
gallevy98 <32230577+gallevy98@users.noreply.github.com>
2019-03-18 15:57:23 -0400

    fixing databse

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   db: {
-    uri: 'mongodb://client:client1@ds011745.mlab.com:11745/tutor_matching_service', //place the URI of your mongo database here.
+    uri: 'mongodb://client:client1@ds127341.mlab.com:27341/twitter_data', //place the URI of your mongo database here.
   },
   port: process.env.PORT || 8080
 };
