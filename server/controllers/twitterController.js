130605777945bac2027ab5b766acdb17ca81f455
Novacane01 <bigchill22@bellsouth.net>
2019-03-31 18:29:42 -0400

    Refined location search

--- server/controllers/twitterController.js
+++ server/controllers/twitterController.js
@@ -15,19 +15,25 @@ const client = new Twitter({
 // });
 
 findCountryByName = (name,placeType,callback)=>{
-   Location.findOne({Name:name, PlaceType:placeType}).then((location)=>{
-    callback(location['WOE_ID'].toString());
+   Location.findOne({Name: new RegExp(`[${name}]`,'i'), PlaceType:placeType}).then((location)=>{
+     if(location == null){
+       return callback(new Error('Location not found'));
+     }
+    callback(null,location['WOE_ID'].toString());
    }),(err)=>{
-      if(err) throw err;
+      if(err) callback(err);
    }
 };
 
 exports.getTrendsAt = (req,res)=>{
-  findCountryByName(req.query.name,req.query.placeType,(woe_id)=>{
+  findCountryByName(req.query.name,req.query.placeType,(err,woe_id)=>{
+    if(err) return res.status(404).send(err.message);
     client.get('https://api.twitter.com/1.1/trends/place.json',{id:woe_id},(err,response,body)=>{
+      console.log(woe_id);
+      console.log(body.body);
       console.log(JSON.parse(body.body)[0].trends);
       // console.log(body);
-      return res.status(200).send(body);
+      return res.status(200).send(JSON.parse(body.body)[0].trends);
     });
     // return res.status(404).send();
   });
