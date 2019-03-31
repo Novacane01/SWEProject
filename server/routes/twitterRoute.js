d404f92aaf3c4dc5623a54323103e5cde9cd71a2
Novacane01 <bigchill22@bellsouth.net>
2019-03-31 17:46:39 -0400

    Added get trending tweets by location

--- server/routes/twitterRoute.js
+++ server/routes/twitterRoute.js
@@ -1,6 +1,6 @@
 const twitterController = require('../controllers/twitterController'), 
     router = require('express').Router();
 
-router.route('/location/:loc').get(twitterController.findCountryByName);
-
+// router.route('/location/:loc').get(twitterController.findCountryByName);
+router.route('/trendingAt').get(twitterController.getTrendsAt);
 module.exports = router;
