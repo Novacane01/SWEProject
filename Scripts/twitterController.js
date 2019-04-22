801158dc595386308efbe12fe318a1eee2c5f989
Novacane01 <bigchill22@bellsouth.net>
2019-04-22 16:29:36 -0400

    added graphs

--- Scripts/twitterController.js
+++ Scripts/twitterController.js
@@ -33,6 +33,7 @@ angular.module('trends').controller('TwitterController',['$scope', 'Trends', '$s
                             return b.tweet_volume-a.tweet_volume;
                         });
                         $scope.trending = trends;
+                        $scope.updateGraph();
                     }
                 },(err)=>{
                     console.log('Unable to retrieve trends:',err);
@@ -63,6 +64,47 @@ angular.module('trends').controller('TwitterController',['$scope', 'Trends', '$s
                 }
             },(err)=>{
                 console.log(err);
+            });
+        }
+        $scope.updateGraph = ()=>{
+            var trendNames = [];
+            var trendData = [];
+            $scope.trending.forEach((element)=>{
+                if(element.tweet_volume>0){
+                    trendNames.push(element.name);
+                    trendData.push(element.tweet_volume);
+                }
+            });
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var chart = new Chart(ctx, {
+              // The type of chart we want to create
+              type: 'line',
+              // The data for our dataset
+              data: {
+                  labels: trendNames,
+                  datasets: [{
+                      label: 'Tweet Volume',
+                      backgroundColor: 'rgba(145, 205, 255,.5)',
+                      borderColor: 'rgb(145, 205, 255)',
+                      data: trendData
+                  }]
+              },
+          
+              // Configuration options go here
+              options: {
+                layout: {
+                    padding: {
+                        left: 50,
+                        right: 50,
+                        top: 0,
+                        bottom: 0
+                        }
+                    },
+                    scales: {
+                        yAxes: [{ticks: {fontSize: 12, fontFamily: "'Montserrat', sans-serif", fontColor: '#000', fontStyle: '500'}}],
+                        xAxes: [{ticks: {fontSize: 12, fontFamily: "'Montserrat', sans-serif", fontColor: '#000', fontStyle: '500'}}]
+                    }
+                }
             });
         }
         $scope.updateTrends("United States",$scope.type);
