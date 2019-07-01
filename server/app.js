78c700c90cac8673251b4584f45de6e6e9bfc433
Novacane01 <bigchill22@bellsouth.net>
2019-07-01 12:17:37 -0400

    Fix missing CORS header

--- server/app.js
+++ server/app.js
@@ -9,6 +9,11 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use((req, res, next)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static('Views'));
 app.use(express.static('Scripts'));
 app.use(express.static('Resources'));
